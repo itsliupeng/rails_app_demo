58f54f1160bc3ccca115fb1fafa72a2a147260c9
liupeng <itsliupeng@gmail.com>
2015-07-05 22:03:37 +0800

    finish nginx & unicorn conf

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+ working_directory "/home/work/AppDemo"
+ pid '/home/work/AppDemo/tmp/pids/unicorn.pid'
+ stderr_path '/home/work/AppDemo/log/unicorn.log'
+ stdout_path '/home/work/AppDemo/log/unicorn.log'
+
+ listen '/tmp/unicorn.todo.sock'
+ worker_processes 2
+ timeout 30
