1a24f0ac03e470aac5484c04db285f6d9f814070
liupeng <itsliupeng@gmail.com>
2015-07-06 08:17:05 +0800

    finished capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,18 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
+# server "192.168.33.11", :web, :app, :db, primary: true
+
 set :application, 'app_demo.com'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'https://github.com/itsliupeng/rails_app_demo.git'
+set :user, "work"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :barnch, 'master'
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -23,10 +27,10 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -35,9 +39,15 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :keep_releases, 5
 
 namespace :deploy do
+	# %w[ start stop restart ].each do |command|
+	# 	desc "#{command} unicon server"
+	# 	task command, roles: :app, except: {no_release: true} do
+	# 		run "/etc/init.d/unicorn_#{application} #{command}"
+	# 	end	
+	# end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
+	after :restart, :clear_cache do
+		on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
